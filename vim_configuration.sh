0b03e4bc54b367bd7c4ec08255b539afc29e6d46
spyton <shiyangfrance@hotmail.com>
2017-09-30 16:24:37 +0200

    30/09/2017

---
+++ vim_configuration.sh
@@ -0,0 +1,19 @@
+ #!/bin/bash
+ # vim plugin install script 
+
+ # need wget and unzip packages
+
+
+
+wget -O bash-support.zip http://www.vim.org/scripts/download_script.php?src_id=9890
+
+test -f ~/.vim || mkdir ~/.vim # if the directory does not exist already
+unzip bash-support.zip -d ~/.vim/
+
+test -f ~/.vimrc || mkdir ~/.vimrc
+echo "filetype plugin on" >> ~/.vimrc
+
+
+echo "let g:BASH_AuthorName   = \'Yang\'" >> ~/.vimrc
+echo "let g:BASH_Email        = \'shiyangfrance@hotmail.com\'" >> ~/.vimrc
+echo "let g:BASH_Company      = \'Devoteam\'">> ~/.vimrc
